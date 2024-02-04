01c3b678064b4c4694ddff945936633f5a54b396
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-02-04 17:17:46 +0400

    Update ar4.java

--- ar4.java
+++ ar4.java
@@ -1,19 +1,24 @@
-public class ar4{
-	public static void main (String[] arg){
-		int a[] = {5, 2, 7, 3}; 
-		int x;
-		for (int i=0; i<a.length; i++){
-    			for (int j=1; j<a.length; j++){
-        			if (a[j]<a[j-1]){
-       					x = a[j];
-			       		a[j] = a[j-1];
-          				a[j-1] = x;
-				}
-			}
-				
-		}
-		for (int y:a)
-			System.out.println(y);
-	
+public class ar4 {
+  public static void main(String[] arg) {
+    int a[] = {
+      5,
+      2,
+      7,
+      3
+    };
+    int x;
+    for (int i = 0; i < a.length; i++) {
+      for (int j = 1; j < a.length; j++) {
+        if (a[j] < a[j - 1]) {
+          x = a[j];
+          a[j] = a[j - 1];
+          a[j - 1] = x;
         }
+      }
+
+    }
+    for (int y: a)
+      System.out.println(y);
+
+  }
 }
